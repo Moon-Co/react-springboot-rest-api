a39fd53ef765e0c049d67fca45ef5ac3a2473e05
Moon-Co <tmdtlr34@naver.com>
2022-09-15 18:46:39 +0900

    feat: 기능 구현

--- server/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ server/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -2,16 +2,21 @@ package com.example.gccoffee.repository;
 
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.nio.ByteBuffer;
-import java.security.Timestamp;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
-
+import java.util.*;
+import static com.example.gccoffee.JdbcUtils.toLocalDateTime;
+import static com.example.gccoffee.JdbcUtils.toUUID;
+@Repository
 public class ProductJdbcRepository implements ProductRepository{
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -22,12 +27,18 @@ public class ProductJdbcRepository implements ProductRepository{
 
     @Override
     public List<Product> findAll() {
-        return jdbcTemplate.query("select * from product",productRowMapper);
+        return jdbcTemplate.query("select * from products",productRowMapper);
     }
 
     @Override
     public Product insert(Product product) {
-        return null;
+
+        var update = jdbcTemplate.update("INSERT INTO products(product_id ,product_name ,category ,price ,description ,created_at ,updated_at)"+
+                "VALUES(UUID_TO_BIN(:productId),:productName ,:category ,:price ,:description ,:createdAt ,:updatedAt)",toParamMap(product));
+        if(update != 1){
+            throw new RuntimeException("nothing was inserted");
+        }
+        return product;
     }
 
     @Override
@@ -37,17 +48,37 @@ public class ProductJdbcRepository implements ProductRepository{
 
     @Override
     public Optional<Product> findById(UUID productId) {
-        return Optional.empty();
+        try{
+        return Optional.of(
+                jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = UUID_TO_BIN(:productId)",
+                        Collections.singletonMap("productId", productId.toString().getBytes()),productRowMapper)
+        );
+        }catch(EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
+
     }
 
     @Override
     public Optional<Product> findByName(String productName) {
-        return Optional.empty();
+        try{
+            return Optional.of(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
+                            Collections.singletonMap("productName",productName),productRowMapper)
+            );
+        }catch(EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Product> findByCategory(Category category) {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT * FROM products WHERE category = :category",
+                Collections.singletonMap("category",category.toString()),
+                productRowMapper
+        );
+
     }
 
     @Override
@@ -58,11 +89,11 @@ public class ProductJdbcRepository implements ProductRepository{
     private static final RowMapper<Product> productRowMapper =(resultSet,i)->{
         var productId = toUUID(resultSet.getBytes("product_id"));
         var productName =resultSet.getString("product_name");
-        var category     =Category.valueOf("category");
+        var category     =Category.valueOf(resultSet.getString("category"));
         var price        =resultSet.getLong("price");
         var description  =resultSet.getString("description");
         var createdAt   = toLocalDateTime(resultSet.getTimestamp("created_at"));
-        var updatedAt   =resultSet.getString("updated_at");
+        var updatedAt   =toLocalDateTime(resultSet.getTimestamp("updated_at"));
         return new Product(
                 productId,
                 productName,
@@ -72,12 +103,20 @@ public class ProductJdbcRepository implements ProductRepository{
                 createdAt,
                 updatedAt
         );
+    };
+    private Map<String, Object> toParamMap(Product product) {
+        var paramMap = new HashMap<String, Object>();
+        paramMap.put("productId",product.getProductId().toString().getBytes());
+        paramMap.put("productName",product.getProductName());
+        paramMap.put("category",product.getCategory().toString());
+        paramMap.put("price",product.getPrice());
+        paramMap.put("description",product.getDescription());
+        paramMap.put("createdAt",product.getCreatedAt());
+        paramMap.put("updatedAt",product.getUpdatedAt());
+        return paramMap;
     }
-    static UUID toUUID(byte[] bytes){
-        var byteBuffer = ByteBuffer.wrap(bytes);
-        return new UUID(byteBuffer.getLong(),byteBuffer.getLong());
-    }
-    static LocalDateTime toLocalDateTime(Timestamp timestamp){
-        return timestamp!=null ? timestamp.toLocalDateTime() : null;
-    }
+
+
+
+
 }
